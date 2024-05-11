fa768e7bfe91ab54cd3317d5e932c14e10fabfeb
HeJia <jiahe634@gmail.com>
2024-05-11 17:37:09 +0800

    注释对象,所有富文本对象都包含一个 annotations 对象，用于设置富文本的样式。

---
+++ src/main/java/com/lucky/notionapi/model/block/richtext/Annotation.java
@@ -0,0 +1,45 @@
+package com.lucky.notionapi.model.block.richtext;
+
+import com.lucky.notionapi.enumerate.Color;
+import lombok.Data;
+
+/**
+ * 注释对象
+ * 所有富文本对象都包含一个 Annotation 对象，用于设置富文本的样式。
+ * @author jiahe
+ */
+@Data
+public class Annotation {
+
+    /**
+     * 文本是否加粗。
+     */
+    private Boolean bold;
+
+    /**
+     * 文本是否斜体。
+     */
+    private Boolean italic;
+
+    /**
+     * 文本是否被删除。
+     */
+    private Boolean strikethrough;
+
+    /**
+     * 文本是否带下划线。
+     */
+    private Boolean underline;
+
+    /**
+     * 文本是否为代码类型。
+     */
+    private Boolean code;
+
+    /**
+     * 文本的颜色。
+     * @see Color
+     */
+    private Boolean color;
+
+}
