8b3304affd2041f84c4dbfa1544a5f837ffd6af5
贺佳 <hejia1744709356@163.com>
2024-05-12 16:03:13 +0800

    用户对象

--- src/main/java/com/lucky/notionapi/model/user/User.java
+++ src/main/java/com/lucky/notionapi/model/user/User.java
@@ -1,12 +1,12 @@
 package com.lucky.notionapi.model.user;
 
 
-import com.lucky.notionapi.model.block.AbstractBlockChild;
+import com.lucky.notionapi.model.block.AbstractBlock;
 
 /**
  * User 对象代表 Notion 工作区中的用户。用户包括完整的工作区成员、访客和集成。
  * 用户对象几乎出现在 API 返回的所有对象中，包括：
- * 1.块对象{@link AbstractBlockChild}的{@link AbstractBlockChild#} 和 last_edited_by
+ * 1.块对象{@link AbstractBlock}的{@link AbstractBlock#getCreatedBy()} 和 {@link AbstractBlock#getLastEditedBy()}
  * 2.页面对象中的created_by 和 last_edited_by 以及 people 属性项
  * 3.数据库对象中的created_by 和 last_edited_by
  * 4.富文本对象中的用户提到
