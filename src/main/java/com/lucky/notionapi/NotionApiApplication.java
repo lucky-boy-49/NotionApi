0406c5e69ed092f673f7be62cdcb08add3296f5a
贺佳 <hejia1744709356@163.com>
2024-05-12 16:49:54 +0800

    启动类

---
+++ src/main/java/com/lucky/notionapi/NotionApiApplication.java
@@ -0,0 +1,16 @@
+package com.lucky.notionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 贺佳
+ */
+@SpringBootApplication
+public class NotionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotionApiApplication.class, args);
+    }
+
+}
