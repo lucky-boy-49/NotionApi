f1047772cda245fc8e235b4d84c6040c21549460
HeJia <jiahe634@gmail.com>
2024-05-13 16:03:38 +0800

    属性序列化和反序列化多态

--- src/main/java/com/lucky/notionapi/model/page/properties/Properties.java
+++ src/main/java/com/lucky/notionapi/model/page/properties/Properties.java
@@ -1,10 +1,41 @@
 package com.lucky.notionapi.model.page.properties;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.lucky.notionapi.model.page.properties.type.*;
+
+import static com.lucky.notionapi.model.page.properties.Properties.*;
+
 /**
  * 页属性接口
  *
  * @author 贺佳
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = CheckboxProperties.class, name = CHECKBOX),
+        @JsonSubTypes.Type(value = CreatedByProperties.class, name = CREATED_BY),
+        @JsonSubTypes.Type(value = CreatedByProperties.class, name = CREATED_TIME),
+        @JsonSubTypes.Type(value = DateProperties.class, name = DATE),
+        @JsonSubTypes.Type(value = EmailProperties.class, name = EMAIL),
+        @JsonSubTypes.Type(value = FileProperties.class, name = FILES),
+        @JsonSubTypes.Type(value = FormulaProperties.class, name = FORMULA),
+        @JsonSubTypes.Type(value = LastEditedByProperties.class, name = LAST_EDITED_BY),
+        @JsonSubTypes.Type(value = LastEditedTimeProperties.class, name = LAST_EDITED_TIME),
+        @JsonSubTypes.Type(value = MultiSelectProperties.class, name = MULTI_SELECT),
+        @JsonSubTypes.Type(value = NumberProperties.class, name = NUMBER),
+        @JsonSubTypes.Type(value = PeopleProperties.class, name = PEOPLE),
+        @JsonSubTypes.Type(value = PhoneNumberProperties.class, name = PHONE_NUMBER),
+        @JsonSubTypes.Type(value = RelationProperties.class, name = RELATION),
+        @JsonSubTypes.Type(value = RollupProperties.class, name = ROLLUP),
+        @JsonSubTypes.Type(value = RichTextProperties.class, name = RICH_TEXT),
+        @JsonSubTypes.Type(value = SelectProperties.class, name = SELECT),
+        @JsonSubTypes.Type(value = StatusProperties.class, name = STATUS),
+        @JsonSubTypes.Type(value = TitleProperties.class, name = TITLE),
+        @JsonSubTypes.Type(value = URLProperties.class, name = URL),
+        @JsonSubTypes.Type(value = UniqueIDProperties.class, name = UNIQUE_ID),
+        @JsonSubTypes.Type(value = VerificationProperties.class, name = VERIFICATION)
+})
 public interface Properties {
 
     /**
