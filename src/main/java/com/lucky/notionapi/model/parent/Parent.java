126f753bb716db366b09ecaf36d949275696c354
HeJia <jiahe634@gmail.com>
2024-05-13 15:54:24 +0800

    父对象

--- src/main/java/com/lucky/notionapi/model/parent/Parent.java
+++ src/main/java/com/lucky/notionapi/model/parent/Parent.java
@@ -1,5 +1,13 @@
 package com.lucky.notionapi.model.parent;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.lucky.notionapi.model.parent.type.BlockParent;
+import com.lucky.notionapi.model.parent.type.DatabaseParent;
+import com.lucky.notionapi.model.parent.type.PageParent;
+import com.lucky.notionapi.model.parent.type.WorkspaceParent;
+
 /**
  * 页面、数据库和块要么位于其他页面、数据库和块内部，要么位于工作区的顶层。该位置称为“父位置”。父级信息由整个 API 中一致的 parent 对象表示。
  * 1.页面可以由其他页面、数据库、块或整个工作区作为父级。
@@ -7,6 +15,13 @@ package com.lucky.notionapi.model.parent;
  * 3.数据库可以按页面、块或整个工作区作为父级。
  * @author jiahe
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION, property = "type")
+@JsonSubTypes({
+        @Type(value = BlockParent.class, name = "block_id"),
+        @Type(value = DatabaseParent.class, name = "database_id"),
+        @Type(value = PageParent.class, name = "page_id"),
+        @Type(value = WorkspaceParent.class, name = "workspace")
+})
 public interface Parent {
 
     /**
