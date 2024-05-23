b0e75d97a30b72ac2cdced4f13a373dbb115c0b1
HeJia <jiahe634@gmail.com>
2024-05-23 12:05:19 +0800

    页块设置childPage字段对应Json的“child_page”节点

--- src/main/java/com/lucky/notionapi/model/block/type/ChildPageBlock.java
+++ src/main/java/com/lucky/notionapi/model/block/type/ChildPageBlock.java
@@ -1,6 +1,7 @@
 package com.lucky.notionapi.model.block.type;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lucky.notionapi.model.block.AbstractBlock;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,6 +20,7 @@ public class ChildPageBlock extends AbstractBlock {
      * 要创建或更新 child_page 类型块，请使用创建页面和更新页面端点，
      * 并在 parent 正文参数中指定父页面的 ID。
      */
+    @JsonProperty("child_page")
     private ChildPage childPage;
 
     public ChildPageBlock() {
